4c7c786266703efdd9aab3a0d93b846d829b0422
dymyw <dymayongwei@163.com>
2021-08-06 10:49:11 +0800

    feat: client RPC call

--- examples/rpccall.go
+++ examples/rpccall.go
@@ -1,6 +1,9 @@
 package main
 
+// todo test
+
 import (
+    "encoding/gob"
     "fmt"
     "github.com/dymyw/simple-rpc/internal/client"
     "github.com/dymyw/simple-rpc/internal/server"
@@ -8,12 +11,35 @@ import (
     "time"
 )
 
+// User1 传输对象
+type User1 struct {
+    Name string
+    Age int
+}
+
+// UserDB1 实例
+var userDB1 = map[int]User1{
+    3: User1{"张三", 23},
+    6: User1{"赵六", 66},
+    5: User1{"王五", 35},
+}
+
+// QueryUser1 执行方法
+func QueryUser1(id int) (User1, error) {
+    if u, ok := userDB1[id]; ok {
+        return u, nil
+    }
+
+    return User1{}, fmt.Errorf("id %d not in user db", id)
+}
+
 func main() {
-    addr := "localhost:9009"
+    gob.Register(User1{})
+    addr := "localhost:9008"
     // 创建服务
     srv := server.NewServer(addr)
     // 服务注册
-    srv.Register("QueryUser", QueryUser)
+    srv.Register("QueryUser", QueryUser1)
     // 服务启动
     go srv.Run()
 
@@ -29,12 +55,12 @@ func main() {
     cli := client.NewClient(conn)
 
     // 桩代码
-    var Query func(int) (User, error)
+    var Query func(int) (User1, error)
     // 客户端打桩
     cli.CallPRC("QueryUser", &Query)
 
     // 远程调用
-    u, err := Query(5)
+    u, err := Query(6)
     if err != nil {
         panic(err)
     }
--- internal/client/client.go
+++ internal/client/client.go
@@ -6,6 +6,9 @@ package client
 //  id, err := rpcTest(10)
 
 import (
+    "errors"
+    "github.com/dymyw/simple-rpc/internal/dataserial"
+    "github.com/dymyw/simple-rpc/internal/transport"
     "net"
     "reflect"
 )
@@ -27,9 +30,72 @@ func (c *Client) CallPRC(rpcName string, fPtr interface{}) {
 
     // 桩代码
     f := func(req []reflect.Value) []reflect.Value {
-        // todo
+        // 请求错误处理
+        errorHandler := func(err error) []reflect.Value {
+            // 出参
+            numOut := container.Type().NumOut()
+            outArgs := make([]reflect.Value, numOut)
 
-        return []reflect.Value{}
+            for i := 0; i < len(outArgs)-1; i++ {
+                outArgs[i] = reflect.Zero(container.Type().Out(i))
+            }
+            outArgs[len(outArgs)-1] = reflect.ValueOf(&err).Elem()
+
+            return outArgs
+        }
+
+        // 参数
+        inArgs := make([]interface{}, 0, len(req))
+        for _, arg := range req {
+            // 转换
+            inArgs = append(inArgs, arg.Interface())
+        }
+
+        // 请求
+        reqData := dataserial.RPCdata{Name: rpcName, Args: inArgs}
+        b, err := dataserial.Encode(reqData)
+        if err != nil {
+            panic(err)
+        }
+        cReqTransport := transport.NewTransport(c.conn)
+        err = cReqTransport.Send(b)
+        if err != nil {
+            return errorHandler(err)
+        }
+
+        // 读取
+        rsp, err := cReqTransport.Read()
+        if err != nil {
+            return errorHandler(err)
+        }
+        rspData, _ := dataserial.Decode(rsp)
+        // 错误处理
+        if rspData.Err != "" {
+            return errorHandler(errors.New(rspData.Err))
+        }
+        // 无返回值
+        if len(rspData.Args) == 0 {
+            rspData.Args = make([]interface{}, container.Type().NumOut())
+        }
+
+        // 出参
+        numOut := container.Type().NumOut()
+        outArgs := make([]reflect.Value, numOut)
+        for i := 0; i < numOut; i++ {
+            // 转换
+            if i != numOut-1 {
+                if rspData.Args[i] == nil {
+                    outArgs[i] = reflect.Zero(container.Type().Out(i))
+                } else {
+                    outArgs[i] = reflect.ValueOf(rspData.Args[i])
+                }
+            } else {
+                // Err
+                outArgs[i] = reflect.Zero(container.Type().Out(i))
+            }
+        }
+
+        return outArgs
     }
 
     // 设置反射对象值
